6dbd6e61970def0f4fe13cd1b31d0b4f82b670e1
José Manuel Muñoz <josemanuel.munozba@elorrieta-errekamari.com>
2021-04-25 19:42:11 +0200

    First commit with project connecting to IRC Twitch channel

---
+++ standalone/source/main.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2011 Fredi Machado <https://github.com/fredimachado>
+ * IRCClient is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * http://www.gnu.org/licenses/lgpl.html
+ */
+
+#include "irc/IRCClient.h"
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <signal.h>
+
+void msgCommand(std::string arguments, IRCClient *client) {
+    std::string to = arguments.substr(0, arguments.find(" "));
+    std::string text = arguments.substr(arguments.find(" ") + 1);
+
+    std::cout << "To " + to + ": " + text << std::endl;
+    client->SendIRC("PRIVMSG " + to + " :" + text);
+}
+
+void joinCommand(std::string channel, IRCClient *client) {
+    if (channel[0] != '#')
+        channel = "#" + channel;
+
+    client->SendIRC("JOIN " + channel);
+}
+
+void partCommand(std::string channel, IRCClient *client) {
+    if (channel[0] != '#')
+        channel = "#" + channel;
+
+    client->SendIRC("PART " + channel);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        std::cout << "Insuficient parameters: host port [nick] [user]"
+                  << std::endl;
+        return 1;
+    }
+
+    char *host = argv[1];
+    int port = atoi(argv[2]);
+    std::string nick("chemanumunoz");
+    std::string user("chemanumunoz");
+
+    if (argc >= 4)
+        nick = argv[3];
+    if (argc >= 5)
+        user = argv[4];
+
+    IRCClient client;
+
+    client.Debug(true);
+
+    if (client.InitSocket()) {
+        std::cout << "Socket initialized. Connecting..." << std::endl;
+
+        if (client.Connect(host, port)) {
+            std::cout << "Connected. Loggin in..." << std::endl;
+
+            if (client.Login(nick, user, getenv("TWITCH_TOKEN"))) {
+                std::cout << "Logged." << std::endl;
+
+                client.SendIRC("JOIN #nietmetal");
+
+                while (client.Connected()) {
+                    client.ReceiveData();
+                }
+            }
+
+            if (client.Connected()) {
+                client.Disconnect();
+            }
+
+            std::cout << "Disconnected." << std::endl;
+        }
+    }
+}
